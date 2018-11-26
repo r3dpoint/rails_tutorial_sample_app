7f3d755b5a18c585c52ba45a3c838b1633940c26
redpoint <r3dpoint@users.noreply.github.com>
2018-11-25 19:10:22 -0800

    added staticpages controller

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+# The StaticPagesController controller.
+class StaticPagesController < ApplicationController
+  def home
+    # home
+  end
+
+  def help
+    # help
+  end
+end
