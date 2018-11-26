e7cf974d1157a907f07a828fcdfdd8a56a12ae5c
redpoint <r3dpoint@users.noreply.github.com>
2018-11-25 19:07:02 -0800

    Add hello

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
+# The ApplicationController controller.
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: 'hello, world!'
+  end
 end
