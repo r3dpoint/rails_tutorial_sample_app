b7bfb89ddfcddce9bbcb077a6130710a026d5224
redpoint <r3dpoint@users.noreply.github.com>
2018-11-26 05:21:12 -0800

    fixed root url tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root 'static_pages#home'
   get 'foo/bar'
   get 'foo/baz'
   get 'static_pages/home'
@@ -8,5 +9,4 @@ Rails.application.routes.draw do
   get 'static_pages/about'
   get 'static_pages/contact'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'application#hello'
 end
--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -6,9 +6,23 @@ RSpec.describe StaticPagesController, type: :controller do
   render_views
   let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
+  describe 'GET indezx' do
+    it 'routes to static_pages#home' do
+      # https://relishapp.com/rspec/rspec-rails/v/3-8/docs/routing-specs/route-to-matcher
+      # https://gist.github.com/rafaelfragosom/940adee11ccf39f4a069
+      expect(get: root_url(subdomain: nil)).to route_to(controller: 'static_pages', action: 'home')
+      # puts Rails.application.routes.url_helpers.inspect
+      # get root_url
+      # get root_path
+      # expect(response).to have_http_status(:success)
+      # assert_select 'title', "Home | #{base_title}"
+    end
+  end
+
   describe 'GET #home' do
     it 'returns http success and expected title' do
       get :home
+      # get static_pages_home_url
       expect(response).to have_http_status(:success)
       assert_select 'title', "Home | #{base_title}"
     end
@@ -17,6 +31,7 @@ RSpec.describe StaticPagesController, type: :controller do
   describe 'GET #help' do
     it 'returns http success and expected title' do
       get :help
+      # static_pages_help_url
       expect(response).to have_http_status(:success)
       assert_select 'title', "Help | #{base_title}"
     end
@@ -25,6 +40,7 @@ RSpec.describe StaticPagesController, type: :controller do
   describe 'GET #about' do
     it 'returns http success and expected title' do
       get :about
+      # get static_pages_about_url
       expect(response).to have_http_status(:success)
       assert_select 'title', "About | #{base_title}"
     end
@@ -33,6 +49,7 @@ RSpec.describe StaticPagesController, type: :controller do
   describe 'GET #contact' do
     it 'returns http success and expected title' do
       get :contact
+      # get static_pages_contact_url
       expect(response).to have_http_status(:success)
       assert_select 'title', "Contact | #{base_title}"
     end
