b7c90a16cb31e540bc5aa9d8578b9b0b23083b35
redpoint <r3dpoint@users.noreply.github.com>
2018-11-25 19:16:18 -0800

    added foo controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  get 'foo/bar'
+  get 'foo/baz'
   get 'static_pages/home'
   get 'static_pages/help'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
